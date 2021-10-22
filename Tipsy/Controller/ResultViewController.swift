23e44312805888384b1d1beb8ca7bb67ddaa7bf8
m00racle <yanuar.prakosa@gmail.com>
2021-10-22 08:36:48 +0700

    add ResultViewController

---
+++ Tipsy/Controller/ResultViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by Yanuar Heru on 22/10/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
