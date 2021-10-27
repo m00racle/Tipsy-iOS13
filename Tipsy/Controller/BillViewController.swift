b5623ab7496fff814524bc8f69d69cb3dd3da699
m00racle <yanuar.prakosa@gmail.com>
2021-10-27 13:16:30 +0700

    connect bill View outlets and actions

--- Tipsy/Controller/BillViewController.swift
+++ Tipsy/Controller/BillViewController.swift
@@ -9,7 +9,26 @@
 import UIKit
 
 class BillViewController: UIViewController {
-
+//    outlets:
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+//    actions:
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+//        handle stepper value changing
+    }
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+//        handle each button changed and handles other tip buttons.
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+//        handle calculation
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
